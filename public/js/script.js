30de3d650ea49f2f8bb572fa811e19091c1d1d73
Austin Mazur <mazuraustin1@gmail.com>
2023-04-18 20:22:13 +0200

    First commit

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("shopping-cart-crud JS imported successfully!");
+});
